952a360f07588632bdcdb1fe227d4bd31dac2372
Mikooboy <mikolaasanen@hotmail.com>
2022-10-06 16:43:27 +0300

    added login

--- ventilation-UI/main.js
+++ ventilation-UI/main.js
@@ -5,15 +5,38 @@ const mqtt = require('mqtt');
 const client = mqtt.connect('mqtt://192.168.83.223:1883');
 
 const express = require('express');
+const basicAuth = require('express-basic-auth');
+const crypto = require('crypto');
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
 
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('./ventilation.db', sqlite3.OPEN_READWRITE, (err) => {
     if (err) throw err;
 });
 
-//db.run(`CREATE TABLE ventilation(id UNSIGNED INTEGER AUTO_INCREMENT PRIMARY KEY, timestamp DATETIME NOT null DEFAULT(CURRENT_TIMESTAMP), nr, speed, setpoint, pressure, auto, error, co2, rh, temp)`);
+//db.run(`CREATE TABLE ventilation(id INTEGER PRIMARY KEY, timestamp DATETIME NOT null DEFAULT(CURRENT_TIMESTAMP), nr, speed, setpoint, pressure, auto, error, co2, rh, temp)`);
+//db.run(`CREATE TABLE login(id UNSIGNED INTEGER AUTO_INCREMENT PRIMARY KEY, timestamp DATETIME NOT null DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, username, logincount)`);
+
+const users = {
+    'test': 'd31aceb3c06e3e7acc5acbf53f7a26a578873b547a1932b2c30358c0d1a710e9'
+}
+
+function encrypt(pass) {
+    return crypto.pbkdf2Sync(pass, 'salt', 100000, 32, 'sha512').toString('hex');
+}
+
+function validate(username, password) {
+    return (users[username] === encrypt(password));
+}
+
+app.use(basicAuth({
+    challenge: true,
+    authorizer: validate
+}));
 
 let statusMessages = [];
 
@@ -47,7 +70,7 @@ client.on('message', function (topic, message) {
 });
 
 app.get('/update', (req, res) => {
-    db.all(`SELECT * FROM ventilation ORDER BY nr DESC LIMIT 1`,function(err, result) {
+    db.all(`SELECT * FROM ventilation ORDER BY id DESC LIMIT 1`,function(err, result) {
         if (err) console.log(err);
         res.send(JSON.stringify(result[0]));
     });
@@ -57,6 +80,10 @@ app.post('/send', (req, res) => {
     console.log(req.body);
     client.publish("controller/settings", JSON.stringify(req.body));
     res.send("Published settings");
+});
+
+app.get('/logout', function (req, res) {
+    res.status(401).send("<h4>Logged out!</h4><a href='/'>Home</a>");
 });
 
 app.get('/*', (req, res) => {
