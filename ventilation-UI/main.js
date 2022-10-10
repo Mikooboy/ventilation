c0cfab0c0587d6b73d188ba754ec4209534da82d
Mikooboy <mikolaasanen@hotmail.com>
2022-10-10 16:01:51 +0300

    added ventilation.db

--- ventilation-UI/main.js
+++ ventilation-UI/main.js
@@ -19,11 +19,12 @@ const db = new sqlite3.Database('./ventilation.db', sqlite3.OPEN_READWRITE, (err
 });
 
 //db.run(`CREATE TABLE ventilation(id INTEGER PRIMARY KEY, timestamp DATETIME NOT null DEFAULT(CURRENT_TIMESTAMP), nr, speed, setpoint, pressure, auto, error, co2, rh, temp)`);
-//db.run(`CREATE TABLE login(id UNSIGNED INTEGER AUTO_INCREMENT PRIMARY KEY, timestamp DATETIME NOT null DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, username, logincount)`);
+//db.run(`CREATE TABLE login(id UNSIGNED INTEGER AUTO_INCREMENT PRIMARY KEY, timestamp DATETIME NOT null DEFAULT CURRENT_TIMESTAMP, username)`);
 
 const users = {
     'test': 'd31aceb3c06e3e7acc5acbf53f7a26a578873b547a1932b2c30358c0d1a710e9'
 }
+let loggedIn = []
 
 function encrypt(pass) {
     return crypto.pbkdf2Sync(pass, 'salt', 100000, 32, 'sha512').toString('hex');
@@ -86,7 +87,8 @@ app.get('/logout', function (req, res) {
     res.status(401).send("<h4>Logged out!</h4><a href='/'>Home</a>");
 });
 
-app.get('/*', (req, res) => {
+app.get('/', (req, res) => {
+
     res.sendFile(path.join(__dirname, 'page.html'));
 });
 
