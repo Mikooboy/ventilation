d577770b3a5e4a8a21a9f7dd95a8b3caae6508a4
Mikooboy <mikolaasanen@hotmail.com>
2022-10-12 16:11:17 +0300

    html changes

--- ventilation-UI/main.js
+++ ventilation-UI/main.js
@@ -8,6 +8,7 @@ const express = require('express');
 const basicAuth = require('express-basic-auth');
 
 const app = express();
+app.use(express.static(__dirname));
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true
@@ -88,7 +89,7 @@ app.get('/data/login', function (req, res) {
    });
 });
 
-app.get('/update', function (req, res) {
+app.get('/data/update', function (req, res) {
     db.all(`SELECT * FROM ventilation ORDER BY id DESC LIMIT 1`,function(err, result) {
         if (err) console.log(err);
         res.send(JSON.stringify(result[0]));
